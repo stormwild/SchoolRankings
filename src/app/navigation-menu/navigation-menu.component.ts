dbdf58dd46bdf51d9a108b38cbac09f6ca139a4a
rishimehta365 <rishimehta365@gmail.com>
2019-04-06 00:54:06 +0530

    <<push changes>> prfl btn intro'ed hdr <dev_1.0.2>

--- src/app/navigation-menu/navigation-menu.component.ts
+++ src/app/navigation-menu/navigation-menu.component.ts
@@ -13,7 +13,7 @@ import { AngularStickyThingsModule } from '../directives/angular-sticky-things.m
 @Component({
   selector: 'app-navigation-menu',
   templateUrl: './navigation-menu.component.html',
-  styleUrls: ['./navigation-menu.component.css']
+  styleUrls: ['./navigation-menu.component.scss']
 })
 export class NavigationMenuComponent implements OnInit {
 
