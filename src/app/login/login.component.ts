117cb747ee97da410c00b5f3ff69b094853e7ac7
Alexander Torrijos <artorrijos@gmail.com>
2019-07-28 14:01:30 +0800

    Local temp changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import {  Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/_services/auth.service';
 import { User } from '../model/user.model';
@@ -9,18 +9,18 @@ import { User } from '../model/user.model';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  
-  isUser: boolean= false;
+
+  isUser = false;
   user: User;
 
   constructor(private router: Router, public auth: AuthService) {
     this.user = JSON.parse(localStorage.getItem('user'));
-    if(this.user!=null){
+    if (this.user != null) {
       this.isUser = true;
     }
-   }
+  }
 
-  
+
   ngOnInit() {
 
     console.log('User: ', this.user);
